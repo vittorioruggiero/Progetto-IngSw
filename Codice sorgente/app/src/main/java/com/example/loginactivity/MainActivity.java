7f797467322328424bcb7a271929c18147ca12f8
Aniello <80123678+aniello19@users.noreply.github.com>
2023-02-01 12:55:12 +0100

    updated LoginActivity and added RegistratiActivity
    codificato login fittizio con username: admin e password: admin
    resa cliccabile la parola "Registrati" che porta alla nuova RegistratiActivity

--- Codice sorgente/app/src/main/java/com/example/loginactivity/MainActivity.java
+++ Codice sorgente/app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -1,29 +1,67 @@
 package com.example.loginactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-//Prova
+
+    EditText nomeUtenteEditText, passwordEditText;
+    TextView campiErratiTextView, registratiTextView;
+    Button accediButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button accediButton = (Button) findViewById(R.id.accediButton);
+        registratiTextView = (TextView) findViewById(R.id.registratiTextView);
+        String registratiText = "Hai un ristorante? Registrati";
+
+        SpannableString spannableString = new SpannableString(registratiText);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistratiActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        };
+
+        spannableString.setSpan(clickableSpan, 19,29, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        registratiTextView.setText(spannableString);
+        registratiTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Login();
+    }
+
+    
+    void Login(){
+        
+        nomeUtenteEditText = (EditText) findViewById(R.id.nomeUtenteEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        campiErratiTextView = (TextView) findViewById(R.id.campiErratiTextView);
+        accediButton = (Button) findViewById(R.id.accediButton);
         accediButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, EmptyActivity.class);
-                MainActivity.this.startActivity(i);
+                
+                if(nomeUtenteEditText.getText().toString().equals("admin") && passwordEditText.getText().toString().equals("admin")){
+                    Intent intent = new Intent(MainActivity.this, EmptyActivity.class);
+                    MainActivity.this.startActivity(intent);
+                }else{
+                    campiErratiTextView.setVisibility(View.VISIBLE);
+                }
             }
         });
-
+        
     }
 }
---
+++ Codice sorgente/app/src/main/java/com/example/loginactivity/RegistratiActivity.java
@@ -0,0 +1,14 @@
+package com.example.loginactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistratiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrati);
+    }
+}
