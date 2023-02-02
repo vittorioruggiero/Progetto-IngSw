75c5ec8c290553678522a2dd4c71538f5a38af94
Aniello <80123678+aniello19@users.noreply.github.com>
2023-02-02 17:01:01 +0100

    progetto ricreato

---
+++ CodiceSorgente/app/src/main/java/com/example/ratatouille23/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.example.ratatouille23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText nomeUtenteEditText, passwordEditText;
+    TextView campiErratiTextView, registratiTextView;
+    Button accediButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registratiTextView = (TextView) findViewById(R.id.registratiTextView);
+        String registratiText = "Hai un ristorante? Registrati";
+
+        SpannableString spannableString = new SpannableString(registratiText);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistratiActivity.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        };
+
+        spannableString.setSpan(clickableSpan, 19,29, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        registratiTextView.setText(spannableString);
+        registratiTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Login();
+    }
+
+
+    void Login(){
+
+        nomeUtenteEditText = (EditText) findViewById(R.id.nomeUtenteEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        campiErratiTextView = (TextView) findViewById(R.id.campiErratiTextView);
+        accediButton = (Button) findViewById(R.id.accediButton);
+        accediButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(nomeUtenteEditText.getText().toString().equals("admin") && passwordEditText.getText().toString().equals("admin")){
+                    Intent intent = new Intent(LoginActivity.this, EmptyActivity.class);
+                    LoginActivity.this.startActivity(intent);
+                }else{
+                    campiErratiTextView.setVisibility(View.VISIBLE);
+                }
+                if(nomeUtenteEditText.getText().toString().equals("user") && passwordEditText.getText().toString().equals("user")){
+                    campiErratiTextView.setVisibility(View.INVISIBLE);
+                    Intent intent = new Intent(LoginActivity.this, ReimpostaPasswordActivity.class);
+                    LoginActivity.this.startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
---
+++ CodiceSorgente/app/src/main/java/com/example/ratatouille23/ReimpostaPasswordActivity.java
@@ -0,0 +1,54 @@
+package com.example.ratatouille23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ReimpostaPasswordActivity extends AppCompatActivity {
+
+    EditText passwordEditText, confermaPasswordEditText;
+    TextView campiNonCompilatiTextView, passwordDiverseTextView;
+    Button confermaButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reimposta_password);
+
+        ResetPassword();
+    }
+
+    void ResetPassword(){
+
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        confermaPasswordEditText = (EditText) findViewById(R.id.confermaPasswordEditText);
+        confermaButton = (Button) findViewById(R.id.confermaButton);
+        campiNonCompilatiTextView = (TextView) findViewById(R.id.campiNonCompilatiTextView);
+        passwordDiverseTextView = (TextView) findViewById(R.id.passwordDiverseTextView);
+
+        confermaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(passwordEditText.getText().toString().equals("") || confermaPasswordEditText.getText().toString().equals("")){
+                    campiNonCompilatiTextView.setVisibility(View.VISIBLE);
+                    passwordDiverseTextView.setVisibility(View.INVISIBLE);
+                }else if(passwordEditText.getText().toString().equals(confermaPasswordEditText.getText().toString())){
+                    Toast.makeText(ReimpostaPasswordActivity.this, "Password cambiata con successo", Toast.LENGTH_SHORT).show();
+                    campiNonCompilatiTextView.setVisibility(View.INVISIBLE);
+                    passwordDiverseTextView.setVisibility(View.INVISIBLE);
+                    Intent intent = new Intent(ReimpostaPasswordActivity.this, LoginActivity.class);
+                    ReimpostaPasswordActivity.this.startActivity(intent);
+                }else{
+                    campiNonCompilatiTextView.setVisibility(View.INVISIBLE);
+                    passwordDiverseTextView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
